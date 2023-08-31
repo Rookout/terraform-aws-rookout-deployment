7b78a8b2e503dfbf71750e250a72640685b23a0e
Gosha <gosha@rookout.com>
2023-08-31 15:34:22 +0300

    feat: added dynatrace oneagent

--- variables.tf
+++ variables.tf
@@ -271,3 +271,16 @@ variable "demo_app_alb_sg_igress_cidr_blocks" {
   default     = ["0.0.0.0/0"]
   description = "Ingress CIDRs datastore's ALB security group"
 }
+
+variable "deploy_dynatrace_agent" {
+  type = bool
+  default = false
+  description = "Flag to deploy dynatrace oneagent inside the application"
+}
+
+variable "dynatrace_pass_token" {
+  type = string
+  default = ""
+  sensitive = true
+  description = "Dynatrace pass token"
+}
